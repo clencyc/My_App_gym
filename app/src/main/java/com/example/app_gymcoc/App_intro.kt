3f6cd9fb4afd461ec8cf91d9e1e989905897dff8
clencyc <clency2023@gmail.com>
2023-05-04 13:00:36 +0300

    App_gym

---
+++ app/src/main/java/com/example/app_gymcoc/App_intro.kt
@@ -0,0 +1,11 @@
+package com.example.app_gymcoc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class App_intro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_intro)
+    }
+}
