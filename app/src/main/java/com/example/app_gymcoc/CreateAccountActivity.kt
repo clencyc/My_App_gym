0ecb4821e744f98ceeb1fdfa3c4cfde2e28ab828
clencyc <clency2023@gmail.com>
2023-05-09 11:03:23 +0300

    Tuesday's commit

--- app/src/main/java/com/example/app_gymcoc/CreateAccountActivity.kt
+++ app/src/main/java/com/example/app_gymcoc/CreateAccountActivity.kt
@@ -1,47 +1,109 @@
 package com.example.app_gymcoc
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import com.google.firebase.auth.FirebaseAuth
 
 class CreateAccountActivity : AppCompatActivity() {
 
-    private lateinit var binding:ActivitySignUpBinding
-    private lateinit var firebaseAuth: FirebaseAuth
+    lateinit var Email_EDT: EditText
+    lateinit var Password_EDT: EditText
+    lateinit var Confirmpass_EDT: EditText
+    lateinit var BTN_Signin: Button
+
+
+    lateinit var auth:FirebaseAuth
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
 
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        auth = FirebaseAuth.getInstance()
 
-        firebaseAuth = FirebaseAuth.getInstance()
+        Email_EDT = findViewById(R.id.emailEt)
+        Password_EDT = findViewById(R.id.passET)
+        Confirmpass_EDT = findViewById(R.id.confirmPassEt)
+        BTN_Signin = findViewById(R.id.btn_signup)
 
-        binding.button.setOnClickListener {
-            val email = binding.emailEt.text.toString()
-            val pass = binding.passET.text.toString()
-            val confirmPass = binding.confirmPassEt.text.toString()
+        auth = FirebaseAuth.getInstance()
 
-            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
-                if (pass == confirmPass) {
 
-                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            val intent = Intent(this, App_intro::class.java)
-                            startActivity(intent)
-                        } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+        BTN_Signin.setOnClickListener {
 
-                        }
-                    }
+            var email = Email_EDT.text.toString().trim()
+            var pass = Password_EDT.text.toString().trim()
+            var confirmpass = Confirmpass_EDT.text.toString().trim()
+
+            //validate
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmpass.isNotEmpty()) {
+                 if (pass == confirmpass) {
+
+                   auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                       val intent = Intent(this, App_intro::class.java)
+                     startActivity(intent)
                 } else {
-                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
-                }
-            } else {
-                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+                  Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
 
-            }
+                }
+                }
+                } else {
+                  Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+                } else {
+                  Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+                 }
+//}
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//binding = ActivitySignUpBinding.inflate(layoutInflater)
+//setContentView(binding.root)
+
+//firebaseAuth = FirebaseAuth.getInstance()
+
+//binding.button.setOnClickListener {
+  //  val email = binding.emailEt.text.toString()
+    //val pass = binding.passET.text.toString()
+    //val confirmPass = binding.confirmPassEt.text.toString()
+
+    //if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+       // if (pass == confirmPass) {
+
+         //   firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+           //     if (it.isSuccessful) {
+             //       val intent = Intent(this, App_intro::class.java)
+               //     startActivity(intent)
+                //} else {
+                  //  Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                //}
+            //}
+        //} else {
+          //  Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+        //}
+    //} else {
+      //  Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+   // }
+//}
--- app/src/main/java/com/example/app_gymcoc/SplashScreenActivity.kt
+++ app/src/main/java/com/example/app_gymcoc/SplashScreenActivity.kt
@@ -3,9 +3,9 @@ package com.example.app_gymcoc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class splashscreenActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splashscreen)
+        setContentView(R.layout.activity_splash_screen)
     }
 }
